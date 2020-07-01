ac6c25dee9b80bc0c2cf544040242b0352914b43
jmandel5 <67707013+jmandel5@users.noreply.github.com>
2020-07-01 14:58:22 -0400

    added my webpage to the nav element

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="car.php">Car</a></li>
-        <li><a href="#">Placeholder</a></li>
+        <li><a href="vangogh.php">Vincent Van Gogh</a></li>
         <li><a href="#">Placeholder</a></li>
     </ul>
 </nav>
