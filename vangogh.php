f92c033ba76a070e9f014061dfcd99709d268b60
KarmaPanda <desmondlai30@gmail.com>
2020-07-01 15:05:10 -0400

    Added navigation CSS to vangogh.php

--- vangogh.php
+++ vangogh.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<title>Vincent Van Gogh</title>
 	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
 <body>
 	
