ab214bc835c4001c9e6521eb8f3492316d627f1f
KarmaPanda <desmondlai30@gmail.com>
2020-07-01 15:05:56 -0400

    Renamed vangogh.php custom CSS from style.css to vangogh.css

--- vangogh.php
+++ vangogh.php
@@ -3,7 +3,7 @@
 <head> 
 	<meta charset="utf-8">
 	<title>Vincent Van Gogh</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/vangogh.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
 <body>
